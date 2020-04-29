78585c63a4ced446306caed4a5d6939e889174ed
unknown <gyanesh2248@gmail.com>
2020-04-29 15:46:01 +0530

    my first project

---
+++ src/App.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+
+import {CardList} from './components/card-lists/card-list.component';
+import { SearchBox } from './components/search-box/search-box.component';
+import './App.css';
+
+class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      monsters: [],
+      searchField: ''
+    };
+  }
+    componentDidMount(){
+      console.log('dsadasdasdasdas');
+      fetch('https://jsonplaceholder.typicode.com/users') 
+      .then(response => response.json())
+      .then(users => this.setState({monsters: users}));
+    }
+  
+  render(){
+  const { monsters, searchField  } = this.state;
+  // const monsters = this.state.monsters;
+  // const searchField = this.state.searchField;
+  const filteredMonsters = monsters.filter(monster =>
+    monster.name.toLowerCase().includes(searchField.toLowerCase()))
+  return (
+    <div className="App">
+      <SearchBox 
+       placeholder='search monsters'
+       handleChange =  {e => this.setState({searchField: e.target.value})}/> 
+      <CardList monsters = {filteredMonsters} />
+    </div>
+  );
+}
+}
+
+export default App;
+
+/* <header className="App-header">
+<img src={logo} className="App-logo" alt="logo" />
+<p>{this.state.string}</p>
+<button onClick= {() => this.setState({string: 'hello Rajpoot'})}>click me</button>
+</header> */
